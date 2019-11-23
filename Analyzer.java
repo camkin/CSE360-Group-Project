4ea9301fc367ac1f676fed7e43f64691957094a8
Kotori Misaka <51569785+MisakaNA@users.noreply.github.com>
2019-11-23 06:47:58 -0700

    Add files via upload

---
+++ Analyzer.java
@@ -0,0 +1,60 @@
+import java.util.*;
+//import java.lang.*;
+
+public class Analyzer {
+    ArrayList<Double> dataSet = new ArrayList<Double>();
+    Scanner input = new Scanner(System.in);
+
+
+    public void addFromKeyboard(ArrayList<Double> data, double lowerBound, int higherBound){
+        double value = input.nextDouble();
+        if(value > lowerBound && value < higherBound) {
+            data.add(value);
+        }
+    }
+
+    public double[] analyzeData(ArrayList<Double> data){
+        //initialize an array to store calculated values
+        double[] arr = new double[5];
+        Double sum = 0.0;
+        Double median;
+
+        //check if the list is empty
+        if(!data.isEmpty()){
+
+            //first element in return array is # of entries
+            arr[0] = data.size();
+
+            for(Double each: data){
+                sum += each;
+            }
+
+            //second element is mean of the list
+            arr[1] = sum/data.size();
+
+            //sort data set to let first number minimum and last number maximum
+            Collections.sort(data);
+            arr[2] = data.get(1);
+            arr[3] = data.get(data.size() - 1);
+
+            if(data.size() % 2 == 0) {
+                median = data.get(data.size() / 2);
+            }
+            else{
+                median = (data.get(data.size() / 2) + data.get(data.size() / 2 + 1))/2;
+            }
+
+            //fifth element is the median
+            arr[4] = median;
+        }
+        else{
+            //set all element in return array to 0 if the list is empty
+            for(int i = 0; i < 5; i++){
+                arr[i] = 0;
+            }
+        }
+
+        return arr;
+    }
+
+}
